bfd7827ecf334555a50fbf55ba082a367267bb05
Nicolás Frías <nicolas.friasrojas@gmail.com>
2022-12-31 20:37:25 -0300

    fix: See error log on escort profile view

--- services/escort/obtainProfileGet.ts
+++ services/escort/obtainProfileGet.ts
@@ -4,6 +4,8 @@ const obtainProfileGet = async (id: number): Promise<any> => {
   try {
     return profiles.find((profile: any) => profile.id === id);
   } catch (err: any) {
+    console.error(err);
+
     return null;
   }
 };
