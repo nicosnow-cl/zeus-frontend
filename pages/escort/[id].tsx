8b21ba4909f5ab35a7b300dbac7df32b496ed753
Nicolás Frías <nicolas.friasrojas@gmail.com>
2022-12-31 23:44:23 -0300

    fix: See error log on escort profile view

--- pages/escort/[id].tsx
+++ pages/escort/[id].tsx
@@ -1,13 +1,14 @@
 import type { ReactElement } from 'react';
 
+// import EscortSection from '../../components/Escort/EscortSection';
 import profiles from '../../dummy/profiles';
 
 export const getServerSideProps = async () => {
   return { props: { data: profiles[0] } };
 };
 
-const EscortPage = () => {
-  return <h1>asdasdas</h1>;
+const EscortPage = ({ data }: any) => {
+  return <pre>{JSON.stringify(data, null, 2)}</pre>;
 };
 
 EscortPage.getLayout = (page: ReactElement) => page;
