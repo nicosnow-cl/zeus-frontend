d1fbe8a33564969b0de14dcef489148bbf2f7204
Nicolás Frías <nicolas.friasrojas@gmail.com>
2022-12-31 21:47:47 -0300

    fix: See error log on escort profile view

--- pages/escort/[id].tsx
+++ pages/escort/[id].tsx
@@ -1,25 +1,22 @@
 import type { ReactElement } from 'react';
 
-import { obtainProfileGetServerless } from '../api/obtainProfileServerLess';
 import EscortSection from '../../components/Escort/EscortSection';
 import ContentContainer from '../../components/UIElements/ContentContainer';
 import MainContainer from '../../components/UIElements/MainContainer';
 import SimpleNavbar from '../../components/UIElements/SimpleNavbar';
-import ViewLadyImage from '../../components/UIElements/ViewLadyImage';
-import obtainProfileGet from '../../services/escort/obtainProfileGet';
 import profiles from '../../dummy/profiles';
 
-const EscortPage = ({ data }: any) => {
-  return <EscortSection profile={data} />;
-};
-
-export async function getServerSideProps({ req, res, query }: any) {
+export const getServerSideProps = async ({ req, res, query }: any) => {
   const { id } = query;
 
   const profile = profiles.find((profile: any) => profile.id === +id);
 
   return { props: { data: profile || null } };
-}
+};
+
+const EscortPage = ({ data }: any) => {
+  return <EscortSection profile={data} />;
+};
 
 EscortPage.getLayout = (page: ReactElement) => (
   <MainContainer>
