1d73ceec77ad18c83763252714bccfb1d0eb7f55
Nicolás Frías <nicolas.friasrojas@gmail.com>
2023-01-01 04:21:04 -0300

    fix: See error log on escort profile view

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import '../styles/_nprogress.scss';
 import '../styles/index.scss';
 import 'the-new-css-reset/css/reset.css'; // Reset styles
-import { createTheme } from '@mui/material/styles';
+import createTheme from '@mui/material/styles/createTheme';
 import { PaletteMode } from '@mui/material';
 import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
@@ -13,11 +13,11 @@ import type { AppProps } from 'next/app';
 import type { NextPage } from 'next';
 import type { ReactElement, ReactNode } from 'react';
 
-import checkIfIsServer from '../utils/checkIfIsServer';
+// import checkIfIsServer from '../utils/checkIfIsServer';
 import getTheme from '../utils/theme';
 import Loader from '../components/Loader';
 import store, { persistor } from '../redux/store';
-import triggerThemeModeChange from '../utils/triggerThemeModeChange';
+// import triggerThemeModeChange from '../utils/triggerThemeModeChange';
 
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
   getLayout?: (page: ReactElement) => ReactNode;
@@ -27,7 +27,7 @@ type AppPropsWithLayout = AppProps & {
   Component: NextPageWithLayout;
 };
 
-const isServer = checkIfIsServer();
+// const isServer = checkIfIsServer();
 
 NProgress.configure({ showSpinner: false });
 Router.events.on('routeChangeStart', () => NProgress.start());
@@ -35,20 +35,21 @@ Router.events.on('routeChangeComplete', () => NProgress.done());
 Router.events.on('routeChangeError', () => NProgress.done());
 
 export default function App({ Component, pageProps }: AppPropsWithLayout) {
-  const [mode, setMode] = useState<PaletteMode>(
-    !isServer ? (localStorage.getItem('themeMode') as PaletteMode) || 'light' : 'light',
-  );
+  // const [mode, setMode] = useState<PaletteMode>(
+  //   !isServer ? (localStorage.getItem('themeMode') as PaletteMode) || 'light' : 'light',
+  // );
+  const [mode, setMode] = useState<PaletteMode>('light');
 
   const theme = useMemo(() => createTheme(getTheme(mode)), [mode]);
   const getLayout = Component.getLayout ?? ((page) => page);
 
-  if (!isServer) {
-    triggerThemeModeChange(() => {
-      const newMode = (localStorage.getItem('themeMode') as PaletteMode) || 'light';
+  // if (!isServer) {
+  //   triggerThemeModeChange(() => {
+  //     const newMode = (localStorage.getItem('themeMode') as PaletteMode) || 'light';
 
-      setMode(newMode);
-    });
-  }
+  //     setMode(newMode);
+  //   });
+  // }
 
   return (
     <Provider store={store}>
