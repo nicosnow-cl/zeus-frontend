1ed5699c27dbb61784a9662da79cfad590c5283c
Nicolás Frías <nicolas.friasrojas@gmail.com>
2022-12-31 21:07:08 -0300

    fix: See error log on escort profile view

--- services/escort/obtainProfileGet.ts
+++ services/escort/obtainProfileGet.ts
@@ -2,7 +2,137 @@ import profiles from '../../dummy/profiles';
 
 const obtainProfileGet = async (id: number): Promise<any> => {
   try {
-    return profiles.find((profile: any) => profile.id === id);
+    return {
+      active: true,
+      age: 23,
+      description:
+        'Hola, mi nombre es Amber Easton, soy una mujer formalmente educada, elegante y confidente, que proporciona servicios de compañía de alto standing a caballeros distinguidos y con clase. Soy excepcionalmente atractiva a eso debes sumarle mi espontánea y ardiente personalidad, además estoy ubicada en un sector exclusivo y discreto',
+      gender: 'female',
+      id: 1,
+      likes: 5722,
+      location: {
+        name: 'Huechuraba',
+        center: [-33.36572145, -70.6429271186091],
+      },
+      name: 'Amber Easton',
+      nationality: 'Brasileña',
+      price: 185000,
+      rrss: [
+        {
+          type: 'instagram',
+          url: '',
+        },
+        {
+          type: 'whatsapp',
+          url: '',
+        },
+        {
+          type: 'facebook',
+          url: '',
+        },
+      ],
+      services: [],
+      type: 'VIP',
+      videos: [
+        {
+          webm: 'https://www.shutterstock.com/shutterstock/videos/1036079837/preview/stock-footage-hot-woman-posing-in-black-lingerie-body-isolated-image.webm',
+          mp4: 'https://www.shutterstock.com/shutterstock/videos/1036079837/preview/stock-footage-hot-woman-posing-in-black-lingerie-body-isolated-image.mp4',
+        },
+        {
+          webm: 'https://www.shutterstock.com/shutterstock/videos/1067245255/preview/stock-footage-woman-compressing-the-skin-of-the-buttocks-checking-for-cellulite-and-excess-subcutaneous-fat.webm',
+          mp4: 'https://www.shutterstock.com/shutterstock/videos/1067245255/preview/stock-footage-woman-compressing-the-skin-of-the-buttocks-checking-for-cellulite-and-excess-subcutaneous-fat.mp4',
+        },
+        {
+          webm: 'https://www.shutterstock.com/shutterstock/videos/15666505/preview/stock-footage-adorable-blonde-wearing-sexy-lingerie-slow-motion.webm',
+          mp4: 'https://www.shutterstock.com/shutterstock/videos/15666505/preview/stock-footage-adorable-blonde-wearing-sexy-lingerie-slow-motion.mp4',
+        },
+        {
+          webm: 'https://st3.depositphotos.com/10858572/31730/v/600/depositphotos_317304336-stock-video-beautiful-woman-sunbathing-in-the.webm',
+          mp4: 'https://st3.depositphotos.com/10858572/31730/v/600/depositphotos_317304336-stock-video-beautiful-woman-sunbathing-in-the.mp4',
+        },
+        {
+          webm: 'https://st3.depositphotos.com/24244138/32934/v/600/depositphotos_329342756-stock-video-girl-in-sexy-lingerie.webm',
+          mp4: 'https://st3.depositphotos.com/24244138/32934/v/600/depositphotos_329342756-stock-video-girl-in-sexy-lingerie.mp4',
+        },
+        {
+          webm: 'https://st2n.depositphotos.com/5034975/47435/v/600/depositphotos_474358772-stock-video-sensual-naked-woman-in-shower.webm',
+          mp4: 'https://st2n.depositphotos.com/5034975/47435/v/600/depositphotos_474358772-stock-video-sensual-naked-woman-in-shower.mp4',
+        },
+        {
+          webm: 'https://st4n.depositphotos.com/12507692/30741/v/600/depositphotos_307418680-stock-video-woman-taking-a-shower-in.webm',
+          mp4: 'https://st4n.depositphotos.com/12507692/30741/v/600/depositphotos_307418680-stock-video-woman-taking-a-shower-in.mp4',
+        },
+        {
+          webm: 'https://st3n.depositphotos.com/24244138/35093/v/600/depositphotos_350933040-stock-video-sexy-topless-girl-with-bare.webm',
+          mp4: 'https://st3n.depositphotos.com/24244138/35093/v/600/depositphotos_350933040-stock-video-sexy-topless-girl-with-bare.mp4',
+        },
+        {
+          webm: 'https://st2n.depositphotos.com/5034975/47435/v/600/depositphotos_474359232-stock-video-sensual-naked-woman-in-shower.webm',
+          mp4: 'https://st2n.depositphotos.com/5034975/47435/v/600/depositphotos_474359232-stock-video-sensual-naked-woman-in-shower.mp4',
+        },
+      ],
+      schedule: {
+        monday: '5pm - 10pm',
+        tuesday: '3pm - 10pm',
+        wednesday: 'No disponible',
+        thursday: '2pm -10pm',
+        friday: '6pm - 10pm',
+        saturday: 'No disponible',
+        sunday: '7pm - 10pm',
+      },
+      avatarImg: {
+        lq: 'https://s3.eu-central-1.amazonaws.com/findpornface/preview/a0bedb90-0e86-4f74-a06f-71b1aad3c006-9178fe4f-87d5-4b44-b12e-b3646aa5a584.jpg',
+        hq: 'https://s3.eu-central-1.amazonaws.com/findpornface/full/2a5a9e85-b688-42bc-b68e-7f55c142d514-f35cbc86-2d7d-473d-9d2d-9ff94aa99364.jpg',
+      },
+      bannerImg:
+        'https://www.shutterstock.com/shutterstock/photos/618826757/display_1500/stock-photo-sexy-woman-silhouette-dancing-at-the-hotel-pole-dancer-female-stripper-in-the-night-sensual-red-618826757.jpg',
+      images: [
+        {
+          lq: 'https://pornstarbyface.com/ImgBase/Galleries/Amber%20Easton/2/1.jpg',
+          hq: 'https://pornstarbyface.com/ImgBase/Galleries/Amber%20Easton/2/1.jpg',
+        },
+        {
+          lq: 'https://pornstarbyface.com/ImgBase/Galleries/Amber%20Easton/3/2.jpg',
+          hq: 'https://pornstarbyface.com/ImgBase/Galleries/Amber%20Easton/3/2.jpg',
+        },
+        {
+          lq: 'https://pornstarbyface.com/ImgBase/Galleries/Amber%20Easton/1/3.jpg',
+          hq: 'https://pornstarbyface.com/ImgBase/Galleries/Amber%20Easton/1/3.jpg',
+        },
+        {
+          lq: 'https://pornstarbyface.com/ImgBase/Galleries/Amber%20Easton/3/4.jpg',
+          hq: 'https://pornstarbyface.com/ImgBase/Galleries/Amber%20Easton/3/4.jpg',
+        },
+        {
+          lq: 'https://pornstarbyface.com/ImgBase/Galleries/Amber%20Easton/1/5.jpg',
+          hq: 'https://pornstarbyface.com/ImgBase/Galleries/Amber%20Easton/1/5.jpg',
+        },
+        {
+          lq: 'https://pornstarbyface.com/ImgBase/Galleries/Amber%20Easton/1/6.jpg',
+          hq: 'https://pornstarbyface.com/ImgBase/Galleries/Amber%20Easton/1/6.jpg',
+        },
+        {
+          lq: 'https://pornstarbyface.com/ImgBase/Galleries/Amber%20Easton/3/7.jpg',
+          hq: 'https://pornstarbyface.com/ImgBase/Galleries/Amber%20Easton/3/7.jpg',
+        },
+        {
+          lq: 'https://pornstarbyface.com/ImgBase/Galleries/Amber%20Easton/3/8.jpg',
+          hq: 'https://pornstarbyface.com/ImgBase/Galleries/Amber%20Easton/3/8.jpg',
+        },
+        {
+          lq: 'https://pornstarbyface.com/ImgBase/Galleries/Amber%20Easton/2/9.jpg',
+          hq: 'https://pornstarbyface.com/ImgBase/Galleries/Amber%20Easton/2/9.jpg',
+        },
+      ],
+      appareance: [
+        'Contextura Delgada',
+        'Cabello Rubio',
+        'Ojos Verdes',
+        'Busto Grande',
+        'Cola Grande',
+      ],
+      phoneNumber: '+569 8571 5634',
+    };
   } catch (err: any) {
     console.error({ err });
 
