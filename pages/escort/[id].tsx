13dd5cc4766e368386e1e76537ca710408ee87e6
Nicolás Frías <nicolas.friasrojas@gmail.com>
2023-01-02 22:41:22 -0300

    fix: Bug on navbar in SSR

--- pages/escort/[id].tsx
+++ pages/escort/[id].tsx
@@ -1,21 +1,25 @@
 import { NextPageWithLayout } from '../_app';
 import { useSelector } from 'react-redux';
 import dynamic from 'next/dynamic';
+import Skeleton from '@mui/material/Skeleton';
 import type { ReactElement } from 'react';
 
 import { RootState } from '../../redux/store';
 import ContentContainer from '../../components/UIElements/ContentContainer';
 import EscortSection from '../../components/Escort/EscortSection';
-import Loader from '../../components/Loader';
 import MainContainer from '../../components/UIElements/MainContainer';
 import obtainProfileGet from '../../services/escort/obtainProfileGet';
 
 const LazyNavbar = dynamic(() => import('../../components/UIElements/Navbar'), {
-  loading: Loader,
+  loading: () => (
+    <Skeleton
+      variant="rectangular"
+      style={{ width: '100%', height: '90px', transform: 'none', position: 'fixed' }}
+    />
+  ),
   ssr: false,
 });
 const LazyMediaDialog = dynamic(() => import('../../components/UIElements/MediaDialog'), {
-  loading: Loader,
   ssr: false,
 });
 
