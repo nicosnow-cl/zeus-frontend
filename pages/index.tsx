e7f7df5b89e5561f209b8f99002cf6430dad62e5
Nicolás Frías <nicolas.friasrojas@gmail.com>
2023-01-05 23:28:45 -0300

    fix: Added lazy load to FiltersModal, RegionsModal and ViewLadiesStories

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,37 @@
 import { NextPageWithLayout } from './_app';
+import { useSelector } from 'react-redux';
+import dynamic from 'next/dynamic';
 import type { ReactElement } from 'react';
 
+// import FiltersModal from '../components/UIElements/FiltersModal';
+// import MediaDialog from '../components/UIElements/MediaDialog';
+// import RegionsModal from '../components/UIElements/RegionsModal';
+// import ViewLadyStory from '../components/UIElements/ViewLadyStory';
+import { RootState } from '../redux/store';
 import checkIfIsServer from '../helpers/checkIfIsServer';
 import ContentContainer from '../components/UIElements/ContentContainer';
 import EscortsSection from '../components/Home/EscortsSection';
-import FiltersModal from '../components/UIElements/FiltersModal';
 import MainContainer from '../components/UIElements/MainContainer';
 import Navbar from '../components/UIElements/Navbar';
 import PageFilters from '../components/UIElements/PageFilters';
-import RegionsModal from '../components/UIElements/RegionsModal';
 import StoriesBar from '../components/Home/StoriesBar';
-import MediaDialog from '../components/UIElements/MediaDialog';
-import ViewLadyStory from '../components/UIElements/ViewLadyStory';
 
 const isServer = checkIfIsServer();
+const LazyRegionsModal = dynamic(() => import('../components/UIElements/RegionsModal'), {
+  ssr: false,
+});
+const LazyFiltersModal = dynamic(() => import('../components/UIElements/FiltersModal'), {
+  ssr: false,
+});
+const LazyViewLadyStory = dynamic(() => import('../components/UIElements/ViewLadyStory'), {
+  ssr: false,
+});
 
 const Home: NextPageWithLayout = () => {
+  const showRegionModal = useSelector((state: RootState): boolean => state.ui.showRegionModal);
+  const showFiltersModal = useSelector((state: RootState): boolean => state.ui.showFiltersModal);
+  const showLadiesStories = useSelector((state: RootState): boolean => state.ui.showLadiesStories);
+
   console.count('Home render');
 
   return (
@@ -27,10 +43,10 @@ const Home: NextPageWithLayout = () => {
       <EscortsSection />
 
       <div>
-        {!isServer && <RegionsModal />}
-        <FiltersModal />
-        <MediaDialog />
-        <ViewLadyStory />
+        {!isServer && showRegionModal && <LazyRegionsModal />}
+        {showFiltersModal && <LazyFiltersModal />}
+        {showLadiesStories && <LazyViewLadyStory />}
+        {/* <MediaDialog /> */}
       </div>
     </div>
   );
