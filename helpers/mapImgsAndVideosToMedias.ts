6cefaa286ef5ee37c846cfe37774cdb6cefdfdba
Nicolás Frías <nicolas.friasrojas@gmail.com>
2023-01-25 23:21:49 -0300

    fix: Fix to generate build

--- helpers/mapImgsAndVideosToMedias.ts
+++ helpers/mapImgsAndVideosToMedias.ts
@@ -1,3 +1,5 @@
+import { ObjectId } from 'mongodb';
+
 import IImage from '../interfaces/objects/interface.image';
 import IMedia from '../interfaces/objects/interface.media';
 import IVideo from '../interfaces/objects/interface.video';
@@ -9,15 +11,25 @@ const mapImgsAndVideosToMedias = (
   order: 'videos_first' | 'images_first' | 'shuffle' = 'shuffle',
 ) => {
   const videosMedia: IMedia[] = videos.map((video, idx) => ({
-    id: idx,
+    _id: new ObjectId(idx).toString(),
+    comments: [],
+    createdAt: new Date().toString(),
+    description: '',
+    escortId: '',
+    likes: 0,
     type: 'video',
     video,
   }));
 
   const imagesMedia: IMedia[] = images.map((img, idx) => ({
-    id: idx + videosMedia.length,
-    type: 'img',
+    _id: new ObjectId(idx + videosMedia.length).toString(),
+    comments: [],
+    createdAt: new Date().toString(),
+    description: '',
+    escortId: '',
     img,
+    likes: 0,
+    type: 'img',
   }));
 
   if (order === 'shuffle') return shuffle([...videosMedia, ...imagesMedia]);
