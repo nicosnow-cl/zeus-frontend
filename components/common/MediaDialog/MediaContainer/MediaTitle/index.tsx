9dc1ce3c63479de2363e92f33de8129201fbcdb6
Nicolás Frías <nicolas.friasrojas@gmail.com>
2023-02-06 23:38:49 -0300

    fix: corrections on media dialog applieds

--- components/common/MediaDialog/MediaContainer/MediaTitle/index.tsx
+++ components/common/MediaDialog/MediaContainer/MediaTitle/index.tsx
@@ -1,9 +1,10 @@
-import { CSSProperties } from 'react';
+import { CSSProperties, useContext } from 'react';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 
 import IImage from '../../../../../interfaces/objects/interface.image';
 import StoryAvatar from '../../../StoryAvatar';
+import { AppContext } from '../../../../../pages/_app';
 
 export interface IMediaTitleProps {
   avatar: IImage;
@@ -13,21 +14,27 @@ export interface IMediaTitleProps {
 }
 
 const MediaTitle = ({ avatar, onClick = () => {}, name, style = {} }: IMediaTitleProps) => {
+  const { device } = useContext(AppContext);
+
+  const isMobile = device?.type === 'mobile';
+  const containerHeight = isMobile ? 55 : 85;
+  const avatarHeight = isMobile ? 50 : 80;
+
   return (
     <div
       className={`w-100 p-3 d-flex fd-row jc-between ai-center`}
-      style={{ height: 85, ...style }}
+      style={{ height: containerHeight, ...style }}
     >
       <div className={`d-flex ai-center col-gap-3`}>
-        <StoryAvatar image={avatar} showBorder={false} size={80} />
+        <StoryAvatar image={avatar} showBorder={false} size={avatarHeight} />
 
-        <Typography variant="h6" color="white">
+        <Typography variant={isMobile ? 'body1' : 'h6'} color="white">
           {name}
         </Typography>
       </div>
 
       {onClick && (
-        <Button onClick={onClick} variant="contained">
+        <Button onClick={onClick} variant="contained" size="small">
           Cerrar
         </Button>
       )}
