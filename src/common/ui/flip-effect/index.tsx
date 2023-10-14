e60d809e7df54a67a3476d2abe17f794a5efaa19
Nicolás Frías <nicolas.friasrojas@gmail.com>
2023-10-14 00:00:52 -0300

    feat: created filp effect component

---
+++ src/common/ui/flip-effect/index.tsx
@@ -0,0 +1,50 @@
+'use client';
+
+import { a, useSpring } from '@react-spring/web';
+import { useEffect, useState } from 'react';
+
+export type FlipEffectProps = {
+  frontChild: React.ReactNode;
+  backChild?: React.ReactNode;
+  delay?: number;
+};
+
+export const FlipEffect = ({ frontChild, backChild, delay = 1000 }: FlipEffectProps) => {
+  const [flipped, setFlipped] = useState(false);
+  const { transform, opacity } = useSpring({
+    opacity: flipped ? 1 : 0,
+    transform: `perspective(600px) rotateY(${flipped ? 180 : 0}deg)`,
+    config: { mass: 5, tension: 500, friction: 80 },
+  });
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => setFlipped(true), delay);
+
+    return () => clearTimeout(timeoutId);
+  }, [delay]);
+
+  return (
+    <div
+      className="relative flex h-fit items-center justify-center"
+      onClick={() => setFlipped((state) => !state)}
+    >
+      <a.div
+        className="absolute h-fit w-full"
+        style={{ opacity: opacity.to((o) => 1 - o), transform }}
+      >
+        {backChild}
+      </a.div>
+
+      <a.div
+        className="h-fit w-full"
+        style={{
+          opacity,
+          transform,
+          rotateY: '180deg',
+        }}
+      >
+        {frontChild}
+      </a.div>
+    </div>
+  );
+};
--- src/features/home/components/cards-container/index.tsx
+++ src/features/home/components/cards-container/index.tsx
@@ -1,26 +1,36 @@
+import { FlipEffect } from '@/common/ui/flip-effect';
 import { getCards } from '../../services/getCards';
 import * as UserCard from '../user-card';
 
 export const CardsContainer = async () => {
   const userCards = await getCards();
 
+  const delays = userCards.map((_, idx) => 1000 + Math.max(1, idx) * 100);
+
   return (
     <>
       {userCards.map((user, idx) => (
-        <UserCard.Root key={idx}>
-          <div className="absolute h-[600px] w-full overflow-hidden">
-            <UserCard.BackgroundMedia avatar={user.avatar} medias={user.medias} />
-          </div>
+        <FlipEffect
+          key={idx}
+          frontChild={
+            <UserCard.Root>
+              <div className="absolute h-[600px] w-full overflow-hidden">
+                <UserCard.BackgroundMedia avatar={user.avatar} medias={user.medias} />
+              </div>
 
-          <UserCard.AvatarWithName
-            avatar={user.avatar}
-            username={user.username}
-            age={user.age}
-            name={user.name}
-          />
-          <UserCard.Description description={user.description} />
-          <UserCard.Actions />
-        </UserCard.Root>
+              <UserCard.AvatarWithName
+                avatar={user.avatar}
+                username={user.username}
+                age={user.age}
+                name={user.name}
+              />
+              <UserCard.Description description={user.description} />
+              <UserCard.Actions />
+            </UserCard.Root>
+          }
+          backChild={<UserCard.Skeleton />}
+          delay={delays[idx]}
+        />
       ))}
     </>
   );
