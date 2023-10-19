58fa8465b2d6501864a7cf5ec01fa2762750d769
Nicolás Frías <nicolas.friasrojas@gmail.com>
2023-10-19 00:16:32 -0300

    fix: minor fix applied

--- src/common/components/ui/others/desktop-navbar/bottom-bar/index.tsx
+++ src/common/components/ui/others/desktop-navbar/bottom-bar/index.tsx
@@ -1,12 +1,12 @@
 'use client'
 
-import { Box, Container, Separator } from '@radix-ui/themes'
+import { Flex, Separator } from '@radix-ui/themes'
 
 import { Breadcrumbs } from '../../breadcrumbs'
 import { ColorModeSwitch } from '../../color-mode-switch'
 import { getUrlBreadcrumb } from '@/common/utils/getUrlBreadcrumb'
 import { LangModeSwitch } from '../../lang-mode-switch'
-import { usePathname } from 'next/navigation'
+import { usePathname } from '@intl/navigation'
 
 export const BottomBar = () => {
   const pathname = usePathname()
@@ -14,19 +14,20 @@ export const BottomBar = () => {
   const crumbs = getUrlBreadcrumb(pathname)
 
   return (
-    <Box
-      pr="2"
-      className={`flex justify-between bg-woodsmoke-200/80 fill-woodsmoke-950 text-woodsmoke-950 backdrop-blur-md backdrop-saturate-150`}
+    <Flex
+      className={`bg-woodsmoke-200/80 fill-woodsmoke-950 text-woodsmoke-950 backdrop-blur-md backdrop-saturate-150`}
+      justify="between"
+      py="1"
+      pl="5"
+      pr="5"
     >
-      <Container py="1" size="4">
-        <Breadcrumbs crumbs={crumbs} />
-      </Container>
+      <Breadcrumbs crumbs={crumbs} />
 
       <span className={`flex items-center gap-x-3`}>
         <ColorModeSwitch />
         <Separator orientation="vertical" size="1" />
         <LangModeSwitch />
       </span>
-    </Box>
+    </Flex>
   )
 }
