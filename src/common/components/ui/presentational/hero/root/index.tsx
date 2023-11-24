3c4fadce61c68a04b9898e6212d0c8d7e006901a
Nicolás Frías <nicolas.friasrojas@gmail.com>
2023-11-24 19:30:00 -0300

    feat: modifications on hero banner

--- src/common/components/ui/presentational/hero/root/index.tsx
+++ src/common/components/ui/presentational/hero/root/index.tsx
@@ -3,8 +3,6 @@ export type RootProps = React.HTMLAttributes<HTMLDivElement>
 export const Root = (props: RootProps) => (
   <section
     {...props}
-    className={`grid-wrapper hero relative h-[550px] border-b-2 border-gray-200 shadow-xl  dark:border-gray-900 ${
-      props?.className ?? ''
-    }`}
+    className={`grid-wrapper hero relative h-[550px] overflow-hidden ${props?.className ?? ''}`}
   />
 )
