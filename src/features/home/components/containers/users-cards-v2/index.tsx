4454538abe8b0675d0e15fe82e6beafaa8743138
Nicolás Frías <nicolas.friasrojas@gmail.com>
2023-12-01 20:16:31 -0300

    feat: start working with css grid

--- src/features/home/components/containers/users-cards-v2/index.tsx
+++ src/features/home/components/containers/users-cards-v2/index.tsx
@@ -1,13 +1,15 @@
 import { Grid } from '@radix-ui/themes'
 import { MotionConfig, PanInfo } from 'framer-motion'
 import { useRef, useState } from 'react'
-import Masonry from 'react-masonry-component'
+// import Masonry from 'react-masonry-component'
+import { XMasonry, XBlock } from 'react-xmasonry'
 
 import { DimLayer } from '@/common/components/ui/presentational/dim-layer'
 import { UserCardEntity } from '@/common/types/entities/user-card-entity.type'
 import { UserCardDynamic } from '../user-card-dynamic'
 import * as UserCardMinimal from '../../ui/presentational/user-card'
 import styles from './styles.module.css'
+import { MasonryContainer } from '@/common/components/containers/masonry'
 
 export type UsersCardsContainerProps = {
   data?: UserCardEntity[]
@@ -44,6 +46,17 @@ export const UsersCardsContainerV2 = ({ data = [] }: UsersCardsContainerProps) =
     }
   }
 
+  const getCardDimensions = (type: string) => {
+    switch (type) {
+      case 'VIP':
+        return { width: 2, height: 500, style: { gridColumn: 'span 3', gridRow: 'span 4' } }
+      case 'PREMIUM':
+        return { width: 1, height: 450, style: { gridColumn: 'span 3', gridRow: 'span 3' } }
+      default:
+        return { width: 1, height: 400, style: { gridColumn: 'span 2', gridRow: 'span 2' } }
+    }
+  }
+
   return (
     <MotionConfig
       transition={{
@@ -53,7 +66,7 @@ export const UsersCardsContainerV2 = ({ data = [] }: UsersCardsContainerProps) =
       }}
     >
       {/* <div className="columns-4 gap-1"> */}
-      <Masonry
+      {/* <Masonry
         className={'grid w-full'}
         options={{
           transitionDuration: 0,
@@ -64,20 +77,44 @@ export const UsersCardsContainerV2 = ({ data = [] }: UsersCardsContainerProps) =
           percentPosition: true,
         }}
       >
-        <div className={'grid-sizer'} />
+        <div className={'grid-sizer'} /> */}
 
-        {data.map((user, idx) => (
-          <div key={idx} className={`grid-item ${getCardSize(user.type)}`}>
-            <UserCardDynamic
-              data={user}
-              expanded={selectedId === user._id}
-              containerProps={{
-                onClick: (evt) => handleClickCard(evt, { idx, id: user._id }),
-              }}
-            />
-          </div>
-        ))}
-      </Masonry>
+      {/* <MasonryContainer
+        masonryOptions={{
+          columnNumber: 4,
+        }}
+      > */}
+      {/* <XMasonry> */}
+      <div
+        className="gap-1"
+        style={{
+          display: 'grid',
+          gridTemplateColumns: 'repeat(auto-fit, minmax(10rem, 1fr))',
+          gridAutoRows: '10rem',
+          gridAutoFlow: 'dense',
+        }}
+      >
+        {data.map((user, idx) => {
+          const { width, height, style } = getCardDimensions(user.type)
+
+          return (
+            // <XBlock key={idx} width={width}>
+            <div key={idx} className={`relative`} style={style}>
+              <UserCardDynamic
+                data={user}
+                expanded={selectedId === user._id}
+                containerProps={{
+                  onClick: (evt) => handleClickCard(evt, { idx, id: user._id }),
+                }}
+              />
+            </div>
+            // </XBlock>
+          )
+        })}
+      </div>
+      {/* </XMasonry> */}
+      {/* </MasonryContainer> */}
+      {/* </Masonry> */}
       {/* </div> */}
 
       <DimLayer
