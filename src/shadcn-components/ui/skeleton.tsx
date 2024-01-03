2ae47e7624eb43c4c927070fde6ad73dd92cc247
Nicolás Frías <nicolas.friasrojas@gmail.com>
2024-01-02 23:04:08 -0300

    fix: all shadcn components reinstalled

--- src/shadcn-components/ui/skeleton.tsx
+++ src/shadcn-components/ui/skeleton.tsx
@@ -1,9 +1,12 @@
-import { cn } from '@/shadcn-lib/utils'
+import { cn } from "@/shadcn-lib/utils"
 
-function Skeleton({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) {
+function Skeleton({
+  className,
+  ...props
+}: React.HTMLAttributes<HTMLDivElement>) {
   return (
     <div
-      className={cn('animate-pulse rounded-md bg-gray-900/10 dark:bg-gray-50/10', className)}
+      className={cn("animate-pulse rounded-md bg-gray-900/10 dark:bg-gray-50/10", className)}
       {...props}
     />
   )
